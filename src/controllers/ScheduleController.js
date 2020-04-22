0b4f691ccd251eb25527cb8834b5311d7b8fc411
guuhfsouza <62706070+guuhfsouza@users.noreply.github.com>
2020-04-21 22:03:07 -0300

    corrção de retorno de pass

--- src/controllers/ScheduleController.js
+++ src/controllers/ScheduleController.js
@@ -35,7 +35,7 @@ module.exports = {
             return response.json({ warning: "O horário já está preenchido. Encontre outro horário"});
 
         try{
-            const response = await connection('Schedule')
+            response = await connection('Schedule')
             .insert({
                 idService,
                 client,
@@ -46,7 +46,7 @@ module.exports = {
                 hour
             })
 
-            return response.json([ response,
+            return response.json([response,
                 { sucess: `Horário do dia ${date} as ${hour} agendado com sucesso para o Sr(a) ${client}`}]);
     
         }
